252484ce38ebeb92cd4d68f3dfc3d33cc8b61a7b
AB786883603 <786883603@qq.com>
2018-11-10 00:28:06 +0800

    添加 MyBatis 拦截器

---
+++ src/main/java/top/Seiei/forMyBatis/interceptor/PageInterceptor.java
@@ -0,0 +1,96 @@
+package top.Seiei.forMyBatis.interceptor;
+
+import java.sql.Connection;
+import java.util.Map;
+import java.util.Properties;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.reflection.DefaultReflectorFactory;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
+import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
+
+import top.Seiei.forMyBatis.pojo.Page;
+
+/*
+ *	MyBatis 分页拦截器
+ *	实现 Mybatis 的 Interceptor 接口
+ * 	选中接口后，按 F2 可搜索其实现类
+ * 
+ */
+
+// MyBatis 的 @Interceptes 注释用于声明要拦截的位置（查看MyBatis源码）
+// @Signature 的 type 指向所要拦截的接口的 Class, method 指向所要拦截的方法, args 是该拦截方法的参数
+// MyBatis 的 StatementHandler 主要用于制作 Statement 对象，即装载 sql 语句
+
+@Intercepts({@Signature(type=StatementHandler.class,method="prepare",args={Connection.class})})
+public class PageInterceptor implements Interceptor{
+
+
+	// 判断是否为要拦截的对象，参数是被检测的对象
+	// 如果不是要拦截的对象，则返回，如果是，则返回达成协议，具有代理权力的代理类
+	// 这里就是拦截要触发 prepare 方法的对象，增删改查都需要这个方法，所以这里只是初步过滤
+	
+	@Override
+	public Object plugin(Object target) {
+		return Plugin.wrap(target, this); // this 就是本拦截器，查看源码
+	}
+	
+	// 参数 invocation 为被拦截下来的对象
+	// 首先，需要进一步筛选，比如分页功能只是查，就要把增删改这些给过滤掉
+	// 如何过滤？ invocation 是被拦截的对象，使用反射就可以获取它的一切信息
+    // 此时只要命名需要分页功能的查询方法名称加入特定的识别字母（如 ByPage 字母作后缀）,进行筛选即可
+	
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		// 获取 Statement
+		StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+		// MyBatis 的包装方法用于反射获取信息
+		MetaObject metaObject = MetaObject.forObject(statementHandler, new DefaultObjectFactory(), new DefaultObjectWrapperFactory(), new DefaultReflectorFactory());
+		// 查看源码，其实先拦截了 RoutingStatementHandler，再通过它的 delegate 访问到 BaseStatementHandler
+		// 而 BaseStatementHandler 通过 MappedStatement 对象访问到具体方法的具体属性
+		MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("delegate.MappedStatement"); // 字符串是要获取信息的 OGNL 表达式
+		// 获取 Dao 接口的名称（sql 的 xml 配置文件中的 id 属性）
+		String id = mappedStatement.getId();
+		// 筛选匹配后
+		if (id.matches(".+ByPage$")) {
+			
+			// 获取 BoundSql 类，它包含 Sql 的信息，因为 BoundSql 类  statementHandler 有直接获取的获取的方法，所以不用反射
+			BoundSql boundSql = statementHandler.getBoundSql();
+			
+			// 获取 sql 参数
+			@SuppressWarnings("unchecked")
+			Map<String, Object> paramsMap = (Map<String, Object>) boundSql.getParameterObject();
+			Page page = (Page) paramsMap.get("page"); // 获取页数对象
+			// 查询拦截数据在数据库中的总条数
+			// 获取当前拦截对象的 Connection 数据库链接
+			Connection connection = (Connection) invocation.getArgs()[0];
+			
+			// 获取原始 sql 语句
+			String sql = boundSql.getSql();
+			
+			
+			// 改造后的 sql 语句
+			String PageSql = null;
+			// 把修改的 sql 语句替代原始的，BoundSql 没有修改 sql 的方法，只能用反射了
+			// OGNL 表达式可以参考上面
+			metaObject.setValue("delegate.boundSql.sql", PageSql);
+		}
+		
+		//返回主权，其实就是重新调用被拦截的方法
+		return invocation.proceed();
+	}
+
+	@Override
+	public void setProperties(Properties properties) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
